6a990c92c2cec590bc47b13be75a456bd1417292
mzanko <125043164+mzanko@users.noreply.github.com>
2024-09-14 17:05:37 +0200

    Add files via upload
    Linq

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,69 @@
+﻿using Model.Klase;
+
+namespace Linq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Podaci podaci = new Podaci();
+            var GrupirajPremaBanci = podaci.ListaKlijenata
+                .Where(x => x.Stanje >= 1000000.0)
+                .GroupBy(x => x.Banka, (banka, osobe) =>
+                new GrupiraniMilijunasi()
+                {
+                    Banka = banka,
+                    Milijunasi = osobe.Select(x => x.ImePrezime)
+                }
+                );
+
+            foreach (var gr in GrupirajPremaBanci)
+            {
+
+                foreach (string mil in gr.Milijunasi)
+                {
+                    var str = gr.Banka + ": " + mil;
+                    Console.WriteLine($"{str}");
+                }
+            }
+
+            var FiltrirajPoBanci = GrupirajPremaBanci.Where(x => x.Banka == "ZABA");
+            foreach (var gr in FiltrirajPoBanci)
+            {
+
+                foreach (string mil in gr.Milijunasi)
+                {
+                    var str = gr.Banka + ": " + mil;
+                    Console.WriteLine($"{str}");
+                }
+            }
+
+            var sortirajPoAbecedi = podaci.ListaKlijenata.OrderBy(x => x.ImePrezime);
+            foreach(var item in sortirajPoAbecedi)
+            {
+                Console.WriteLine(item.ImePrezime);
+            }
+
+            var najBogatiji = podaci.ListaKlijenata.OrderByDescending(x => x.Stanje).FirstOrDefault();
+            Console.WriteLine($"Najbogatiji: {najBogatiji.ImePrezime}");
+
+            IEnumerable<Klijent> Klijents;
+            var GrupirajPremaStanju = podaci.ListaKlijenata
+                .GroupBy(x => x.Stanje, (banka, osobe) =>
+                new
+                {
+                    Banka = banka,
+                    Klijents = osobe.Select(x => x.ImePrezime).ToList(),
+                }
+                );
+            foreach(var item in GrupirajPremaStanju)
+            {
+                foreach (string klijent in item.Klijents)
+                {
+                    var str = item.Banka + ": " + klijent;
+                    Console.WriteLine($"{str}");
+                }
+            }
+        }
+        }
+}
---
+++ Linq/Model/Klase/Podaci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Klase
+{
+    public class Podaci
+    {
+        public List<Banka> ListaBanki = new List<Banka>() 
+        {
+            new Banka() {Simbol = "RBA", Naziv = "Raifeissen Banka"},
+            new Banka() {Simbol = "ZABA", Naziv = "Zagrebacka Banka"},
+            new Banka() {Simbol = "ERST", Naziv = "Erste Banka"}
+        };
+        
+        public List<Klijent> ListaKlijenata = new List<Klijent>()
+        {
+            new Klijent() {ImePrezime = "Pero Peric", Stanje = 1000.0, Banka = "RBA"},
+            new Klijent() {ImePrezime = "Marko Markic", Stanje = 1000000.0, Banka = "RBA"},
+            new Klijent() {ImePrezime = "Ivan Ivic", Stanje = 100000.0, Banka = "RBA"},
+            new Klijent() {ImePrezime = "Mima Mimic", Stanje = 10000000.0, Banka = "ZABA"},
+            new Klijent() {ImePrezime = "Ana Anic", Stanje = 100000.0, Banka = "ZABA"},
+            new Klijent() {ImePrezime = "Jozo Jozic", Stanje = 100.0, Banka = "ZABA"},
+            new Klijent() {ImePrezime = "Ciro Ciric", Stanje = 10000.0, Banka = "ZABA"},
+            new Klijent() {ImePrezime = "Grga Grgic", Stanje = 100000000.0, Banka = "ERST"},
+            new Klijent() {ImePrezime = "Janko Jankic", Stanje = 100.0, Banka = "ERST"},
+            new Klijent() {ImePrezime = "Hrvoje Hrvic", Stanje = 10000000.0, Banka = "ERST"},
+            new Klijent() {ImePrezime = "Ferdo Ferdic", Stanje = 1000.0, Banka = "ERST"},
+            new Klijent() {ImePrezime = "Slavko Slavkic", Stanje = 1000.0, Banka = "RBA"},
+
+        };
+    }
+}
